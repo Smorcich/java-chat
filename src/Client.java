fa04f512c0d9263c52c965fb29dcc0451638da3e
Smorcich <saltimure@yandex.ru>
2020-05-15 19:20:29 +0300

    1.1

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,33 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.*;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		Socket client = new Socket("127.0.0.1", 8000);
 
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        client.getOutputStream()
+                )
+        );
+        
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        client.getInputStream()
+                )
+        );
+        
+        writer.write("q server");
+        writer.flush();
+        
+        client.close();
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,33 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.*;
+
 public class Server {
+
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket server = new ServerSocket(8000);
+		Socket clientSocket = server.accept();
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        clientSocket.getOutputStream()
+                )
+        );
+        
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        clientSocket.getInputStream()
+                )
+        );
+		   
+	    System.out.println(reader.readLine());
+		
+        server.close();
+        clientSocket.close();
+	}
 
 }
